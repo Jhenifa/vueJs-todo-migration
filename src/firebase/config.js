352f97210621627c51825f980d8e22081b567aca
Jhenifa <nnennaezeugwu20@gmail.com>
2025-10-24 09:45:01 +0100

    updated todoList

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth'
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBLhOxXTH3kEjWAxGeJuvASo9LKgf7QnAo",
-    authDomain: "vue-todo-app-d92ec.firebaseapp.com",
-    projectId: "vue-todo-app-d92ec",
-    storageBucket: "vue-todo-app-d92ec.firebasestorage.app",
-    messagingSenderId: "361833013141",
-    appId: "1:361833013141:web:563dc66c477776a3d9609c",
-    measurementId: "G-V3ZVZE5GXB"
+   apiKey: "AIzaSyDiZj4jg8ETzPXshAw0x5I8VuLoOCANdrc",
+  authDomain: "todo-app-b0cc3.firebaseapp.com",
+  projectId: "todo-app-b0cc3",
+  storageBucket: "todo-app-b0cc3.firebasestorage.app",
+  messagingSenderId: "777479335065",
+  appId: "1:777479335065:web:a318ff95b6eda504f6dc08",
+  measurementId: "G-J1F4N1Q4EZ"
   };
 
 
